02e5862e96bfb79577a809e27bfbec995abeda68
azizulmaqsud <azizulmaqsud@gmail.com>
2023-07-24 12:08:53 -0400

    ETL with Glue RDS & RedShift

---
+++ CreateTable.sql
@@ -0,0 +1,17 @@
+Create Table 
+========================
+CREATE TABLE Persons (
+    Id int,
+    Name varchar(255),
+    Email varchar(255)
+);
+
+Insert Table 
+========================
+INSERT INTO MyTable
+  ( Id, Name, Email )
+VALUES
+  (1,'Azizul', 'Virginia Office'), 
+  (2,'Maqsud', 'Virginia Office'), 
+  (3,'Troy', 'Indiana Office'),
+  (4,'Healy', 'DC Office');
